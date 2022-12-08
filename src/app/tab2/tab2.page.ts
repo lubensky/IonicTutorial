d29f2808cba3b79c7a2cf469a77a70ae96d7e4b0
Andreas Lubensky <lubensky@andreas-lubensky.de>
2022-12-08 22:00:12 +0100

    added delete image

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
+import { ActionSheetController } from '@ionic/angular'
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,10 +9,34 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
   async ngOnInit() {
